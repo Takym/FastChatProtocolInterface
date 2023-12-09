6f893f4c6baaa9ef797b5bfe646197ad58ff7b9b
Takym <15681312+Takym@users.noreply.github.com>
2023-12-10 06:52:50 +0900

    デバッグ用のエントリポイントを追加

--- FastChatProtocolInterface/Program.cs
+++ FastChatProtocolInterface/Program.cs
@@ -5,19 +5,32 @@
  * distributed under the MIT License.
 ****/
 
+using System.Runtime.CompilerServices;
+
+#if !DEBUG
 using System;
+#endif
 
 namespace FastChatProtocolInterface
 {
 	internal static class Program
 	{
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		private static void Run(string[] args)
+		{
+			var parsedArgs = CommandLineArguments.ParseArgs(args);
+			parsedArgs.PrintHeader();
+			parsedArgs.ExecutionMode?.Run(parsedArgs);
+		}
+
+#if DEBUG
+		private static void Main(string[] args) => Run(args);
+#else
 		[STAThread()]
 		private static int Main(string[] args)
 		{
 			try {
-				var parsedArgs = CommandLineArguments.ParseArgs(args);
-				parsedArgs.PrintHeader();
-				parsedArgs.ExecutionMode?.Run(parsedArgs);
+				Run(args);
 				return 0;
 			} catch (Exception e) {
 				Console.ForegroundColor = ConsoleColor.Red;
@@ -27,5 +40,6 @@ namespace FastChatProtocolInterface
 				return e.HResult;
 			}
 		}
+#endif
 	}
 }
